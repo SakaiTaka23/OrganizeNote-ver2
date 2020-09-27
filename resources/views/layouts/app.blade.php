5f7d4053185d08806ddd112f4364163314df025d
SakaiTaka23 <tt3.sakai@gmail.com>
2020-09-27 13:02:31 +0900

    ビューの修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -18,6 +19,7 @@
         <!-- Scripts -->
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.6.0/dist/alpine.js" defer></script>
     </head>
+
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             @livewire('navigation-dropdown')
@@ -39,4 +41,5 @@
 
         @livewireScripts
     </body>
+
 </html>
--- resources/views/user/content.blade.php
+++ resources/views/user/content.blade.php
@@ -1,50 +1,56 @@
-@extends('layouts.app_old')
+<x-app-layout>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Content') }}
+        </h2>
+    </x-slot>
 
-            <form method="GET" action="{{ route('searchContent') }}">
-                <div class="form-group row">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
 
-                    <label for="content" class="col-md-4 col-form-label text-md-right">{{ __('目次：') }}</label>
-                    <div class="col-md-6">
-                        <input id="content" type="text" class="form-control" name="content" value="{{ $content ?? '' }}"
-                            autofocus>
+                <form method="GET" action="{{ route('searchContent') }}">
+                    <div class="form-group row">
+
+                        <label for="content" class="col-md-4 col-form-label text-md-right">{{ __('目次：') }}</label>
+                        <div class="col-md-6">
+                            <input id="content" type="text" class="form-control" name="content"
+                                value="{{ $content ?? '' }}" autofocus>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">検索</button>
                     </div>
+                </form>
 
-                    <button type="submit" class="btn btn-primary">検索</button>
-                </div>
-            </form>
+                <a href="{{ route('content') }}">もっと見る</a>
 
-            <a href="{{ route('content') }}">もっと見る</a>
+                <table class="table table-hover mt-2">
+                    <thead class="thead-light">
+                        <tr>
+                            <th>目次</th>
+                            <th>元ページ</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($random_tableofcontents as $content)
+                        @foreach ($content->articles as $article)
 
-            <table class="table table-hover mt-2">
-                <thead class="thead-light">
-                    <tr>
-                        <th>目次</th>
-                        <th>元ページ</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($random_tableofcontents as $content)
-                    @foreach ($content->articles as $article)
+                        <tr>
+                            <td>{{ $content->name }}</td>
+                            <td>{{ $article->title }}</td>
+                            <td><a href="https://note.com/{{ $noteid }}/n/{{ $article->key }}" target="_blank"
+                                    rel="noopener noreferrer">noteで開く</a></td>
+                        </tr>
 
-                    <tr>
-                        <td>{{ $content->name }}</td>
-                        <td>{{ $article->title }}</td>
-                        <td><a href="https://note.com/{{ $noteid }}/n/{{ $article->key }}" target="_blank"
-                                rel="noopener noreferrer">noteで開く</a></td>
-                    </tr>
+                        @endforeach
+                        @endforeach
+                    </tbody>
+                </table>
 
-                    @endforeach
-                    @endforeach
-                </tbody>
-            </table>
-
+            </div>
         </div>
     </div>
-</div>
-@endsection
+
+</x-app-layout>

5e76da07cb64a1dff2acc4bd52ac24ced72864e9
SakaiTaka23 <tt3.sakai@gmail.com>
2020-10-14 15:51:10 +0900

    擬似ユーザー追加

--- database/seeders/user.sql
+++ database/seeders/user.sql
@@ -1,3 +1,4 @@
 INSERT INTO `users` (`id`, `noteid`, `article_count`, `password`, `two_factor_secret`, `two_factor_recovery_codes`, `remember_token`, `current_team_id`, `profile_photo_path`, `created_at`, `updated_at`)
 VALUES
+	(1,'test_user',0,'$2y$10$XMolaMLU78wCl.7IHA8AnOVmAAmM/meQLWs7ItMbD4atu9kWKIRqC',NULL,NULL,NULL,NULL,NULL,'2020-01-01 12:00:00','2020-01-01 12:00:00'),
 	(2,'note_fumi',141,'$2y$10$r8d4v6H06IW/S2vlIFM9bOnDvU1.qdat0mTZfiM8u8sRkRXZCwQXO',NULL,NULL,NULL,NULL,NULL,'2020-08-29 20:59:54','2020-09-07 18:41:18');

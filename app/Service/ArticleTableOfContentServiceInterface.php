78c689dbfc5acc73f2ef3cb314996803a52dbf84
SakaiTaka23 <tt3.sakai@gmail.com>
2021-06-18 11:38:50 +0900

    ファイル名、インターフェース名を同じにして有効なファイルにした

--- app/Service/ArticleTableOfContentServiceInterface.php
+++ app/Service/ArticleTableOfContentServiceInterface.php
@@ -2,6 +2,6 @@
 
 namespace App\Service;
 
-interface ArticleTableOfContent
+interface ArticleTableOfContentServiceInterface
 {
 }
--- app/Service/ArticleTagServiceInterface.php
+++ app/Service/ArticleTagServiceInterface.php
@@ -2,6 +2,6 @@
 
 namespace App\Service;
 
-interface ArticleTag
+interface ArticleTagServiceInterface
 {
 }
